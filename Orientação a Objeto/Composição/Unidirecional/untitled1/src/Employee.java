8c58e30dd34788d0a35b82107ad7ecabf448718d
michael lima <michael.lima.dev@gmail.com>
2025-07-20 10:08:57 -0300

    .

--- Orientação a Objeto/Composição/Unidirecional/untitled1/src/Employee.java
+++
@@ -1,5 +0,0 @@
-public class Employee extends Pessoa{
-    public Employee(String nome, String cpf, String senha) {
-        super(nome, cpf, senha);
-    }
-}
--- Orientação a Objeto/Composição/Unidirecional/untitled1/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args){
-        Sistema sistema = new Sistema();
-
-        do {
-            sistema.principal();
-        }while (true);
-    }
-}
